a568eb18590b8df28acf3c8a6c60f25d9681d08a
Your Name <your@email.com>
2025-11-28 18:27:48 -0500

    review

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,32 @@
 // Footer on JavaScript Form Validation.
 
 document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averageRating: 4.5,
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averageRating: 4.7,
+    },
+    {
+        id: "fs-1987",
+        name: "time-circuits",
+        averageRating: 3.5,
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averageRating: 3.9,
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averageRating: 5.0,
+    }
+]
