a430346e69660562bb660c0e0df844d5dfa29485
Your Name <your@email.com>
2025-11-29 22:35:13 -0500

    d

--- scripts/form.js
+++ scripts/form.js
@@ -30,3 +30,20 @@ const products = [
         averageRating: 5.0,
     }
 ]
+
+
+const rangevalue= 
+document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.range;
+}
+
+
+
