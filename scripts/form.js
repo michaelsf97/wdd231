80ff29f63d0d692ec70df7224bd48fe303f57c92
Your Name <your@email.com>
2025-11-30 21:38:15 -0500

    form

--- scripts/form.js
+++ scripts/form.js
@@ -1,7 +1,6 @@
 // Footer on JavaScript Form Validation.
 
 document.getElementById("currentyear").textContent = new Date().getFullYear();
-const lastModified = document.getElementById("lastModified");
 
 
 const products = [
