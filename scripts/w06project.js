14ad7fd4f6b8fc3e733837b2add4b18fa3acac38
Your Name <your@email.com>
2025-12-04 14:31:43 -0500

    w06

---
+++ scripts/w06project.js
@@ -0,0 +1,9 @@
+//* w06project.js - Project for WDD131 Week 6.
+//* Author: Michael Smith Saavedra Forero
+//* Trycicle WorldWide Ride-Sharing Service
+
+// Set current year in footer
+document.getElementById("currentyear").textcontent = new Date().getFullYear();
+
+// Set last modified date in footer
+document.getElementById("lastModified").textcontent = document.lastModified;
