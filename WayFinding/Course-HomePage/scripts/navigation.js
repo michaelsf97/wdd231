36d3062a9bf734015d461ec5c34d93011bf661f4
Your Name <your@email.com>
2026-01-13 23:53:08 -0500

    commit

--- WayFinding/Course-HomePage/scripts/navigation.js
+++ WayFinding/Course-HomePage/scripts/navigation.js
@@ -17,11 +17,19 @@ const month = document.querySelector('#currentmonth');
 const day = document.querySelector('#day');
 const dayofweek = document.querySelector('#dayofweek');
 
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"];
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 const today = new Date();
+const currentMonth = monthNames[today.getMonth()]; // "January"
+const date = today.getDate(); // 13
+const day = dayNames[today.getDay()]; // "Tuesday"
 
 year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
-month.innerHTML = `getMonth(): <span class="highlight">${today.getMonth()}</span>`;
-day.innerHTML = `getDate(): <span class="highlight">${today.getDate()}</span>`;
-dayofweek.innerHTML = `getDay(): <span class="highlight">${today.getDay()}</span>`;
+month.innerHTML = `getMonth(): <span class="highlight">${currentMonth}</span>`;
+day.innerHTML = `getDate(): <span class="highlight">${date}</span>`;
+dayofweek.innerHTML = `getDay(): <span class="highlight">${day}</span>`;
+
 
 
