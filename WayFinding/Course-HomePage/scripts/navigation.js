2ba31be238b65f6d174deb0f5d22422978246558
Your Name <your@email.com>
2026-01-14 00:07:02 -0500

    navigation

--- WayFinding/Course-HomePage/scripts/navigation.js
+++ WayFinding/Course-HomePage/scripts/navigation.js
@@ -12,24 +12,6 @@ navButton.addEventListener('click', () => {
 });
 
 
-const year = document.querySelector('#currentyear');
-const month = document.querySelector('#currentmonth');
-const day = document.querySelector('#day');
-const dayofweek = document.querySelector('#dayofweek');
-
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"];
-const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-const today = new Date();
-const currentMonth = monthNames[today.getMonth()]; // "January"
-const date = today.getDate(); // 13
-const day = dayNames[today.getDay()]; // "Tuesday"
-
-year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
-month.innerHTML = `getMonth(): <span class="highlight">${currentMonth}</span>`;
-day.innerHTML = `getDate(): <span class="highlight">${date}</span>`;
-dayofweek.innerHTML = `getDay(): <span class="highlight">${day}</span>`;
 
 
 
