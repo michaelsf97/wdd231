cd40ee146e7adde3a7107c283808a191a1c111dd
Your Name <your@email.com>
2026-01-16 00:01:09 -0500

    Clean up WDD 131 files completely

--- scripts/getdates.js
+++
@@ -1,8 +0,0 @@
-// getdates.js - Dynamic date functionality for WDD 131 website
-// Author: Michael Smith Saavedra Forero
-
-// Populate current year in footer
-document.getElementById('currentyear').textContent = new Date().getFullYear();
-
-// Populate last modified date in footer using document.lastModified property
-document.getElementById("lastModified").innerHTML = document.lastModified;
