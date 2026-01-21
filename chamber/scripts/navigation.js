d4728152f1c00de0f18ae9258f70dc29dc6e71c5
Your Name <your@email.com>
2026-01-21 10:12:07 -0500

    larger

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -9,10 +9,17 @@ const navButton = document.querySelector('#hamb-btn');
 const navBar = document.querySelector('#nav-bar');
 
 // Toggle the show class
-
 navButton.addEventListener('click', () => {
     console.log('Clicked');
     navButton.classList.toggle('show');
     navBar.classList.toggle('show');
+});
+
+// Remove show class when resizing to larger screen
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 770) {
+        navButton.classList.remove('show');
+        navBar.classList.remove('show');
+    }
 });
 
