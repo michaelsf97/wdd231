f6ca9b813ab45620b95856bf47348d96c6a8ed09
Your Name <your@email.com>
2026-01-22 07:03:26 -0500

    navigation

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -8,14 +8,15 @@ async function displayMembers() {
         data.members.forEach(member => {
         const memberCard = `
             <div class="member-card">
-                <img src="${data.image}" alt="${data.company_name}">
-                <h3>${data.company_name}</h3>
-                <p><strong>Address:</strong> ${data.company_address}</p>
-                <p><strong>Phone:</strong> ${data.company_phone_number}</p>
-                <p><a href="${data.company_website_url}" target="_blank">Visit website </a></p>
+                <img src="${member.image}" alt="${member.company_name}">
+                <h3>${member.company_name}</h3>
+                <p><strong>Address:</strong> ${member.company_address}</p>
+                <p><strong>Phone:</strong> ${member.company_phone_number}</p>
+                <p><a href="${member.company_website_url}" target="_blank">Visit website </a></p>
             </div>
         `;
                     container.innerHTML += memberCard;
+        });
         
         // Add to page container directly from HTML
         const container = document.querySelector('#members-container');
