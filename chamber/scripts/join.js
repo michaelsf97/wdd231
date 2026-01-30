80f523d23b3f61b54f788d542648b5e7d0c88058
Your Name <your@email.com>
2026-01-30 10:28:46 -0500

    join

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,4 +1,6 @@
+document.querySelector('form').addEventListener('DOMContentLoaded', function() {
 document.querySelector('form').addEventListener('submit', function() {
 
     document.getElementById('timestamp').value = new Date().toISOString();
+});
 });
