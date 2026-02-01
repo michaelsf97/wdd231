133c4e8f870e9b056266b95ece995440286cd2f4
Your Name <your@email.com>
2026-01-31 21:53:20 -0500

    join

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,4 +1,4 @@
-document.querySelector('form').addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
 document.querySelector('form').addEventListener('submit', function() {
 
     document.getElementById('timestamp').value = new Date().toISOString();
