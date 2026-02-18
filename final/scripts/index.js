df414b58766855d732ff931c69d4af2087a72113
Your Name <your@email.com>
2026-02-18 18:28:55 -0500

    index

--- final/scripts/index.js
+++ final/scripts/index.js
@@ -3,20 +3,3 @@ const navBar = document.querySelector('#nav-bar');
 
 
 //
-
-let currentSlide = 0;
-const slides = document.getElementsByClassName('slide');
-
-function showSlide(index) {
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-  slides[index].style.display = 'block';
-}
-
-function changeSlide(direction) {
-  currentSlide += direction;
-  if (currentSlide < 0) currentSlide = slides.length - 1;
-  if (currentSlide >= slides.length) currentSlide = 0;
-  showSlide(currentSlide);
-}
