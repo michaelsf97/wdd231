7c85071b15e669c8748434089c42be5582fae98b
Your Name <your@email.com>
2026-02-19 10:08:34 -0500

    poi

--- final/data/pointsOfInterest.js
+++ final/data/pointsOfInterest.js
@@ -19,13 +19,33 @@ export const pointsOfInterest = [
     },
     {
         name: "Fishtape",
+        toollevel: "low",ntso
         description: "Type of fishtape",
         image: "images/fishtape.webp"
     },
     {
         name: "Electric Planer Tool",
+        toollevel: "Medium",
         description: "Type of electric planer tool",
         image: "images/electricplaner.webp"
     },
+    {
+        name: "Polish",
+        toollevel: "Medium",
+        description: "Polish Machine",
+        image: "images/polish.webp"
+    },
+    {
+        name: "Welding Machine",
+        toollevel: "High",
+        description: "Welding machine to fix up heating",
+        image: "images/weldingmachine.webp"
+    },
+    {
+        name: "Screwdrivers",
+        toollevel: "Low",
+        description: "Screwdrivers box",
+        image: "images/screwdrivers.webp"
+    }
 
 ];
