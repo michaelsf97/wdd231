2fdca61f19761ed241ce89dddcf9b009ad63fa9a
Your Name <your@email.com>
2026-02-19 09:35:00 -0500

    larger

--- final/scripts/index.js
+++ final/scripts/index.js
@@ -1,11 +1,10 @@
-const hamButton = document.getElementById('ham-btn');
-const navBar = document.getElementById('nav-bar');
+const hamButton = document.getElementById('#ham-btn');
+const navBar = document.getElementById('#nav-bar');
 
 if (hamButton && navBar) {
   hamButton.addEventListener('click', () => {
-    console.log('Clicked');
     hamButton.classList.toggle('show');
-    navBar.classList.toggle('show');
+    navBar.classList.toggle('open');
   });
 } else {
   console.error('Missing #ham-btn or #nav-bar in HTML.');
