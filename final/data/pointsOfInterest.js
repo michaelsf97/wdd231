b48c5774cc977d34cd2b7458c954aea3a119ff3c
Your Name <your@email.com>
2026-02-19 07:48:12 -0500

    pointsOfInterest

---
+++ final/data/pointsOfInterest.js
@@ -0,0 +1,31 @@
+export const pointsOfInterest = [
+    {
+        name: "Drill",
+        toollevel: "High",
+        description: "Type of drill",
+        image: "images/taladro.webp",
+    },
+    {
+        name: "Hammers",
+        toollevel: "Low",
+        description: "Type of hammer",
+        image: "images/hammer.webp",
+    },
+    {
+        name: "Chainsaw",
+        toollevel: "High",
+        description: "Type of chainsaw",
+        image: "images/chainsaw.webp",
+    },
+    {
+        name: "Fishtape",
+        description: "Type of fishtape",
+        image: "images/fishtape.webp",
+    },
+    {
+        name: "Electric Planer Tool",
+        description: "Type of electric planer tool",
+        image: "images/electricplaner.webp",
+    },
+    
+]
