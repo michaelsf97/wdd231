1ea952c371ef547a943db218787a8b70e7a63a16
Your Name <your@email.com>
2026-02-19 09:05:19 -0500

    index

--- final/scripts/index.js
+++ final/scripts/index.js
@@ -5,6 +5,6 @@ const navBar = document.getElementById('#nav-bar');
 //
 navButton.addEventListener('click', () => {
     console.log('Clicked');
-    navButton.classList.toggle('open');
+    navButton.classList.toggle('show');
     navBar.classList.toggle('show');
 });
