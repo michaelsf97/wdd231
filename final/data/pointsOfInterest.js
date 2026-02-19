6f93fd2692aa11d50a42f1ac9a8eed635e252cbf
Your Name <your@email.com>
2026-02-19 07:49:34 -0500

    pointsOfInterest

--- final/data/pointsOfInterest.js
+++ final/data/pointsOfInterest.js
@@ -3,29 +3,29 @@ export const pointsOfInterest = [
         name: "Drill",
         toollevel: "High",
         description: "Type of drill",
-        image: "images/taladro.webp",
+        image: "images/taladro.webp"
     },
     {
         name: "Hammers",
         toollevel: "Low",
         description: "Type of hammer",
-        image: "images/hammer.webp",
+        image: "images/hammer.webp"
     },
     {
         name: "Chainsaw",
         toollevel: "High",
         description: "Type of chainsaw",
-        image: "images/chainsaw.webp",
+        image: "images/chainsaw.webp"
     },
     {
         name: "Fishtape",
         description: "Type of fishtape",
-        image: "images/fishtape.webp",
+        image: "images/fishtape.webp"
     },
     {
         name: "Electric Planer Tool",
         description: "Type of electric planer tool",
-        image: "images/electricplaner.webp",
+        image: "images/electricplaner.webp"
     },
-    
-]
+
+];
